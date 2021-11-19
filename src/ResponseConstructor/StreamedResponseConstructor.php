91be507dbb0632f95cb9d4fd6a28376d3b9a7cf4
Christian Kolb <info@digital-craftsman.de>
2021-11-19 12:28:44 +0100

    Add streamed response constructor

---
+++ src/ResponseConstructor/StreamedResponseConstructor.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DigitalCraftsman\CQRS\ResponseConstructor;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+final class StreamedResponseConstructor implements ResponseConstructorInterface
+{
+    /** @param callable $data */
+    public function constructResponse($data, Request $request): Response
+    {
+        return new StreamedResponse($data);
+    }
+}
