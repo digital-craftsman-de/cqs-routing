f0d3907b6c8fb61f8865fe38a76f20c80751b4a9
Christian Kolb <info@digital-craftsman.de>
2022-06-18 17:37:20 +0200

    Remove doctrine bundle

--- tests/Test/config/bundles.php
+++ tests/Test/config/bundles.php
@@ -3,7 +3,6 @@
 return [
     DigitalCraftsman\CQRS\CQRSBundle::class => ['all' => true],
     DigitalCraftsman\Ids\IdsBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
