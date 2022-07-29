90309263f3a1ec05b1eb5771798dfdf4359ae7a5
Christian Kolb <info@digital-craftsman.de>
2022-07-29 18:00:26 +0200

    Add part of the documentation to the interface

--- src/RequestValidator/RequestValidatorInterface.php
+++ src/RequestValidator/RequestValidatorInterface.php
@@ -6,6 +6,14 @@ namespace DigitalCraftsman\CQRS\RequestValidator;
 
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Request validation is there to validate information that is only accessible from the request itself and will not be part of the DTO or
+ * must be validated before a DTO is constructed from the request data.
+ *
+ * Multiple request validators can be applied on each request.
+ *
+ * @see https://github.com/digital-craftsman-de/cqrs/blob/main/docs/request-validator.md
+ */
 interface RequestValidatorInterface
 {
     public function validateRequest(Request $request): void;
