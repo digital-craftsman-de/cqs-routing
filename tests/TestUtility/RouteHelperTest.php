11c74d70a41e4b3d8a5d877084f0b7506bd65012
Christian Kolb <info@digital-craftsman.de>
2025-04-03 13:53:42 +0200

    Add missing coverage

--- tests/TestUtility/RouteHelperTest.php
+++ tests/TestUtility/RouteHelperTest.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace DigitalCraftsman\CQSRouting\TestUtility;
 
+use DigitalCraftsman\CQSRouting\Controller\CommandController;
+use DigitalCraftsman\CQSRouting\Controller\CommandControllerTest;
+use DigitalCraftsman\CQSRouting\Controller\QueryController;
 use DigitalCraftsman\CQSRouting\DTOConstructor\SerializerDTOConstructor;
 use DigitalCraftsman\CQSRouting\RequestDecoder\JsonRequestDecoder;
 use DigitalCraftsman\CQSRouting\ResponseConstructor\EmptyResponseConstructor;
+use DigitalCraftsman\CQSRouting\ResponseConstructor\SerializerJsonResponseConstructor;
 use DigitalCraftsman\CQSRouting\Routing\RouteBuilder;
 use DigitalCraftsman\CQSRouting\Routing\RouteConfigurationBuilder;
 use DigitalCraftsman\CQSRouting\Routing\RoutePayload;
 use DigitalCraftsman\CQSRouting\Test\AppTestCase;
+use DigitalCraftsman\CQSRouting\Test\Domain\Tasks\ReadSide\GetTasks\GetTasksQuery;
+use DigitalCraftsman\CQSRouting\Test\Domain\Tasks\ReadSide\GetTasks\GetTasksQueryHandler;
 use DigitalCraftsman\CQSRouting\Test\Domain\Tasks\WriteSide\CreateTask\CreateTaskCommand;
 use DigitalCraftsman\CQSRouting\Test\Domain\Tasks\WriteSide\CreateTask\CreateTaskCommandHandler;
 use DigitalCraftsman\CQSRouting\Test\Router\RouterFake;
@@ -25,7 +31,7 @@ use Symfony\Component\Routing\RouteCollection;
 final class RouteHelperTest extends AppTestCase
 {
     #[Test]
-    public function get_route_configuration_works(): void
+    public function get_route_configuration_works_for_query(): void
     {
         // -- Arrange
         $routeCollection = new RouteCollection();
@@ -34,7 +40,43 @@ final class RouteHelperTest extends AppTestCase
             new Route(
                 path: '/api/test-route',
                 defaults: [
-                    '_controller' => 'TestController',
+                    '_controller' => QueryController::class,
+                    RouteBuilder::ROUTE_PAYLOAD_KEY => RoutePayload::generatePayload(
+                        dtoClass: GetTasksQuery::class,
+                        handlerClass: GetTasksQueryHandler::class,
+                        requestDecoderClass: JsonRequestDecoder::class,
+                        dtoConstructorClass: SerializerDTOConstructor::class,
+                        responseConstructorClass: SerializerJsonResponseConstructor::class,
+                    ),
+                ],
+            ),
+        );
+
+        $router = new RouterFake($routeCollection);
+
+        $routeHelper = new RouteHelper(
+            router: $router,
+            routeConfigurationBuilder: $this->getContainerService(RouteConfigurationBuilder::class),
+        );
+
+        // -- Act
+        $routeConfiguration = $routeHelper->getRouteConfiguration('test_route');
+
+        // -- Assert
+        self::assertSame(GetTasksQuery::class, $routeConfiguration->dtoClass);
+    }
+
+    #[Test]
+    public function get_route_configuration_works_for_command(): void
+    {
+        // -- Arrange
+        $routeCollection = new RouteCollection();
+        $routeCollection->add(
+            'test_route',
+            new Route(
+                path: '/api/test-route',
+                defaults: [
+                    '_controller' => CommandController::class,
                     RouteBuilder::ROUTE_PAYLOAD_KEY => RoutePayload::generatePayload(
                         dtoClass: CreateTaskCommand::class,
                         handlerClass: CreateTaskCommandHandler::class,
